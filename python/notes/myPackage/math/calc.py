5cf8c0ce27331cc2921c2a2da43ff7da94ed3968
EHwooKim <ehwoo0707@naver.com>
2019-07-23 17:46:33 +0900

    190723 | python - day6 | module, exception

---
+++ python/notes/myPackage/math/calc.py
@@ -0,0 +1,26 @@
+def sum(a, b):
+    """
+    두 수를 더합니다.
+    """
+    return a + b
+
+
+def sub(a, b):
+    """
+    두 수를 뻅니다.
+    """
+    return a - b
+
+def mul(a, b):
+    """
+    두 수를 곱합니다
+    """
+    return a * b
+
+def div(a, b):
+    """
+    두 수를 나눕니다.
+    """
+    if b == 0:
+        return False
+    return a / b
